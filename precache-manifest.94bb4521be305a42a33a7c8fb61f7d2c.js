c2d8e8de4601b2652a5d3de1de64638b85792678
navajreddy.vemula@gmail.com <navajreddy.vemula@gmail.com>
2020-05-23 19:05:48 +0530

    Updates

---
+++ precache-manifest.94bb4521be305a42a33a7c8fb61f7d2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860c3f51312ff7aba96d2662273c843a",
+    "url": "/sampleapp/index.html"
+  },
+  {
+    "revision": "0b2b2fd89d191b859ed7",
+    "url": "/sampleapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70c04998bf88133c79ed",
+    "url": "/sampleapp/static/js/2.61765a83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sampleapp/static/js/2.61765a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2b2fd89d191b859ed7",
+    "url": "/sampleapp/static/js/main.7c84afc9.chunk.js"
+  },
+  {
+    "revision": "629b18a4d0fc71ab4cb2",
+    "url": "/sampleapp/static/js/runtime-main.33cb3123.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sampleapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
